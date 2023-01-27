61cb6d718018cf891888c04b373d0d750f06013c
cerezojerson <123730639+cerezojerson@users.noreply.github.com>
2023-01-27 18:36:24 +0800

    Delete TemperatureConverter.java

--- TemperatureConverter.java
+++
@@ -1,8 +0,0 @@
-public class TemperatureConverter {
-    public static void main(String[] args) {
-        double fahrenheit = 212;
-        double celsius = (fahrenheit - 32) * 5 / 9;
-        System.out.println("Fahrenheit: " + fahrenheit);
-        System.out.println("Celsius: " + celsius);
-    }
-}
