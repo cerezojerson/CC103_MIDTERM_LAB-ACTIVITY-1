b808b83f424322227e3410bf50508e076b91f04a
cerezojerson <123730639+cerezojerson@users.noreply.github.com>
2023-01-27 18:37:50 +0800

    Create GreatestNumber.java

---
+++ GreatestNumber.java
@@ -0,0 +1,14 @@
+public class GreatestNumber {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 78;
+        int c = 87;
+        if (a > b && a > c) {
+            System.out.println("The greatest number is " + a);
+        } else if (b > a && b > c) {
+            System.out.println("The greatest number is " + b);
+        } else {
+            System.out.println("The greatest number is " + c);
+        }
+    }
+}
